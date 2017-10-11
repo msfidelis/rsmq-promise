3b6a572c66858ad2b2dccd4b5e704805cf185749
Matheus Fidelis <msfidelis@outlook.com>
2017-10-11 11:49:26 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const RedisSMQ = require('rsmq');
+const Promise = require('bluebird');
+
+class RedisSMQPromise {
+
+    constructor(options) {
+        this.rsmq = new RedisSMQ(options);
+    }
+
+    get listQueues() {
+        return Promise.promisify(this.rsmq.listQueues);
+    }
+
+    get createQueue() {
+        return Promise.promisify(this.rsmq.createQueue);
+    }
+
+    get deleteQueue() {
+        return Promise.promisify(this.rsmq.deleteQueue);
+    }
+
+    get sendMessage() {
+        return Promise.promisify(this.rsmq.sendMessage);
+    };
+
+    get receiveMessage() {
+        return Promise.promisify(this.rsmq.receiveMessage);
+    };
+
+    get deleteMessage() {
+        return Promise.promisify(this.rsmq.deleteMessage);
+    };
+}
+
+
+module.exports = RedisSMQPromise;
